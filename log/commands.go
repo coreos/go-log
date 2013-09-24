6ccb8074bfabd3092bcd342273af34a9aed52e2c
David Fisher <ddf1991@gmail.com>
2013-09-24 14:14:34 -0700

    Make log functions accept ...interface{} instead of string

--- log/commands.go
+++ log/commands.go
@@ -21,17 +21,17 @@ import (
 )
 
 var BasicFormat = "%s [%9s] %s- %s\n"
-var BasicFields = []string{"time", "priority", "prefix", "message"}
+var BasicFields = []string{"time", "priority", "prefix", "v..."}
 var RichFormat = "%s [%9s] %d %s - %s:%s:%d - %s\n"
-var RichFields = []string{"full_time", "priority", "seq", "prefix", "filename", "funcname", "lineno", "message"}
+var RichFields = []string{"full_time", "priority", "seq", "prefix", "filename", "funcname", "lineno", "v..."}
 
 // This function has an unusual name to aid in finding it while walking the
 // stack. We need to do some dead reckoning from this function to access the
 // caller's stack, so there is a consistent call depth above this function.
-func (logger *Logger) Log(priority Priority, message string) {
+func (logger *Logger) Log(priority Priority, v ...interface{}) {
 	fields := logger.fieldValues()
 	fields["priority"] = priority
-	fields["message"] = message
+	fields["v..."] = fmt.Sprint(v...)
 	for _, sink := range logger.sinks {
 		sink.Log(fields)
 	}
@@ -42,115 +42,115 @@ func (logger *Logger) Logf(priority Priority, format string, v ...interface{}) {
 }
 
 
-func (logger *Logger) Emergency(message string) {
-	logger.Log(PriEmerg, message)
+func (logger *Logger) Emergency(v ...interface{}) {
+	logger.Log(PriEmerg, v...)
 }
-func (logger *Logger) Emergencyf(format string, v...interface{}) {
+func (logger *Logger) Emergencyf(format string, v ...interface{}) {
 	logger.Log(PriEmerg, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Alert(message string) {
-	logger.Log(PriAlert, message)
+func (logger *Logger) Alert(v ...interface{}) {
+	logger.Log(PriAlert, v...)
 }
-func (logger *Logger) Alertf(format string, v...interface{}) {
+func (logger *Logger) Alertf(format string, v ...interface{}) {
 	logger.Log(PriAlert, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Critical(message string) {
-	logger.Log(PriCrit, message)
+func (logger *Logger) Critical(v ...interface{}) {
+	logger.Log(PriCrit, v...)
 }
-func (logger *Logger) Criticalf(format string, v...interface{}) {
+func (logger *Logger) Criticalf(format string, v ...interface{}) {
 	logger.Log(PriCrit, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Error(message string) {
-	logger.Log(PriErr, message)
+func (logger *Logger) Error(v ...interface{}) {
+	logger.Log(PriErr, v...)
 }
-func (logger *Logger) Errorf(format string, v...interface{}) {
+func (logger *Logger) Errorf(format string, v ...interface{}) {
 	logger.Log(PriErr, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Warning(message string) {
-	logger.Log(PriWarning, message)
+func (logger *Logger) Warning(v ...interface{}) {
+	logger.Log(PriWarning, v...)
 }
-func (logger *Logger) Warningf(format string, v...interface{}) {
+func (logger *Logger) Warningf(format string, v ...interface{}) {
 	logger.Log(PriWarning, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Notice(message string) {
-	logger.Log(PriNotice, message)
+func (logger *Logger) Notice(v ...interface{}) {
+	logger.Log(PriNotice, v...)
 }
-func (logger *Logger) Noticef(format string, v...interface{}) {
+func (logger *Logger) Noticef(format string, v ...interface{}) {
 	logger.Log(PriNotice, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Info(message string) {
-	logger.Log(PriInfo, message)
+func (logger *Logger) Info(v ...interface{}) {
+	logger.Log(PriInfo, v...)
 }
-func (logger *Logger) Infof(format string, v...interface{}) {
+func (logger *Logger) Infof(format string, v ...interface{}) {
 	logger.Log(PriInfo, fmt.Sprintf(format, v...))
 }
 
-func (logger *Logger) Debug(message string) {
-	logger.Log(PriDebug, message)
+func (logger *Logger) Debug(v ...interface{}) {
+	logger.Log(PriDebug, v...)
 }
-func (logger *Logger) Debugf(format string, v...interface{}) {
+func (logger *Logger) Debugf(format string, v ...interface{}) {
 	logger.Log(PriDebug, fmt.Sprintf(format, v...))
 }
 
 
-func Emergency(message string) {
-	defaultLogger.Log(PriEmerg, message)
+func Emergency(v ...interface{}) {
+	defaultLogger.Log(PriEmerg, v...)
 }
-func Emergencyf(format string, v...interface{}) {
+func Emergencyf(format string, v ...interface{}) {
 	defaultLogger.Log(PriEmerg, fmt.Sprintf(format, v...))
 }
 
-func Alert(message string) {
-	defaultLogger.Log(PriAlert, message)
+func Alert(v ...interface{}) {
+	defaultLogger.Log(PriAlert, v...)
 }
-func Alertf(format string, v...interface{}) {
+func Alertf(format string, v ...interface{}) {
 	defaultLogger.Log(PriAlert, fmt.Sprintf(format, v...))
 }
 
-func Critical(message string) {
-	defaultLogger.Log(PriCrit, message)
+func Critical(v ...interface{}) {
+	defaultLogger.Log(PriCrit, v...)
 }
-func Criticalf(format string, v...interface{}) {
+func Criticalf(format string, v ...interface{}) {
 	defaultLogger.Log(PriCrit, fmt.Sprintf(format, v...))
 }
 
-func Error(message string) {
-	defaultLogger.Log(PriErr, message)
+func Error(v ...interface{}) {
+	defaultLogger.Log(PriErr, v...)
 }
-func Errorf(format string, v...interface{}) {
+func Errorf(format string, v ...interface{}) {
 	defaultLogger.Log(PriErr, fmt.Sprintf(format, v...))
 }
 
-func Warning(message string) {
-	defaultLogger.Log(PriWarning, message)
+func Warning(v ...interface{}) {
+	defaultLogger.Log(PriWarning, v...)
 }
-func Warningf(format string, v...interface{}) {
+func Warningf(format string, v ...interface{}) {
 	defaultLogger.Log(PriWarning, fmt.Sprintf(format, v...))
 }
 
-func Notice(message string) {
-	defaultLogger.Log(PriNotice, message)
+func Notice(v ...interface{}) {
+	defaultLogger.Log(PriNotice, v...)
 }
-func Noticef(format string, v...interface{}) {
+func Noticef(format string, v ...interface{}) {
 	defaultLogger.Log(PriNotice, fmt.Sprintf(format, v...))
 }
 
-func Info(message string) {
-	defaultLogger.Log(PriInfo, message)
+func Info(v ...interface{}) {
+	defaultLogger.Log(PriInfo, v...)
 }
-func Infof(format string, v...interface{}) {
+func Infof(format string, v ...interface{}) {
 	defaultLogger.Log(PriInfo, fmt.Sprintf(format, v...))
 }
 
-func Debug(message string) {
-	defaultLogger.Log(PriDebug, message)
+func Debug(v ...interface{}) {
+	defaultLogger.Log(PriDebug, v...)
 }
-func Debugf(format string, v...interface{}) {
+func Debugf(format string, v ...interface{}) {
 	defaultLogger.Log(PriDebug, fmt.Sprintf(format, v...))
 }
